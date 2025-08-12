25cc0f009a048025cada823145f2cc5e72264d9e
roor48 <kmgoio@naver.com>
2025-08-13 02:58:00 +0900

    Init

---
+++ src/main/java/org/mingun/modding/AutoAttack.java
@@ -0,0 +1,10 @@
+package org.mingun.modding;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoAttack implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+	}
+}
